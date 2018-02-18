f14cc837522342da0f96985aea261a28c1cecd89
Akhil2453 <akhil.menon94@gmail.com>
2018-02-18 17:59:11 +0530

    Third attempt with SQLite

---
+++ Notepad3/notepad/src/main/java/com/akhil/notepad3/notepad/MyNotes.java
@@ -0,0 +1,12 @@
+package com.akhil.notepad3.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
